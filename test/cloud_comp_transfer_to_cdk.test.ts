0014ed4a17c1fc8b1208753dddf79a938596a50f
anamrzv <ana.mrzv@mail.ru>
2024-10-30 13:42:54 +0100

    Initial commit

---
+++ test/cloud_comp_transfer_to_cdk.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CloudCompTransferToCdk from '../lib/cloud_comp_transfer_to_cdk-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cloud_comp_transfer_to_cdk-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CloudCompTransferToCdk.CloudCompTransferToCdkStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
